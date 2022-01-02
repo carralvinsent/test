3830cd47d06d3450df72f3bcd09a61de62d4eb34
Carral Vincent <36396006+carralvinsent@users.noreply.github.com>
2022-01-02 08:17:52 -0800

    Add files via upload

---
+++ demo/src/main/java/com/drinks/demo/DrinksApplication.java
@@ -0,0 +1,21 @@
+package com.drinks.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinksApplication.class, args);
+	}
+
+
+
+
+
+
+
+
+
+}
---
+++ demo/src/test/java/com/drinks/demo/DrinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinks.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
